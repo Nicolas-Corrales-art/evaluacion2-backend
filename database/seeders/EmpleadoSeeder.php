c892a52120fe701e3665ed98c2cef1b614f14134
Nicolas <nicolas.corrales@uab.edu.bo>
2025-10-27 21:20:58 -0400

    feat(api):crear seeder EmpeladoSeeder

--- database/seeders/EmpleadoSeeder.php
+++ database/seeders/EmpleadoSeeder.php
@@ -30,9 +30,57 @@ class EmpleadoSeeder extends Seeder
              Empleado::create([
             'nombre'=>'Micaela',
             'apellido'=>'Flores',
-            'correo'=>'@gmail.com',
-            'salario'=>3500,
+            'correo'=>'micaela@gmail.com',
+            'salario'=>9500,
            ]);
+              
+
+             Empleado::create([
+            'nombre'=>'Raul',
+            'apellido'=>'Flores',
+            'correo'=>'raul@gmail.com',
+            'salario'=>8500,
+           ]);
+              
+
+             Empleado::create([
+            'nombre'=>'Carlos',
+            'apellido'=>'Lopez',
+            'correo'=>'carlos@gmail.com',
+            'salario'=>3700,
+           ]);   
+
+             Empleado::create([
+            'nombre'=>'Alberto',
+            'apellido'=>'Rojas',
+            'correo'=>'alberto@gmail.com',
+            'salario'=>3506,
+           ]);
+               Empleado::create([
+            'nombre'=>'Juan',
+            'apellido'=>'Rojas',
+            'correo'=>'juan@gmail.com',
+            'salario'=>6500,
+           ]);
+               Empleado::create([
+            'nombre'=>'David',
+            'apellido'=>'Rojas',
+            'correo'=>'david@gmail.com',
+            'salario'=>500,
+           ]);
+                Empleado::create([
+            'nombre'=>'Erick',
+            'apellido'=>'Rojas',
+            'correo'=>'erick@gmail.com',
+            'salario'=>1500,
+           ]);
+                 Empleado::create([
+            'nombre'=>'Angel',
+            'apellido'=>'Rojas',
+            'correo'=>'angel@gmail.com',
+            'salario'=>9500,
+           ]);
+
 
          
     }
